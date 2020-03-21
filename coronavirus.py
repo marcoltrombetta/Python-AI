32121f3562e5bad8d3976be3811bcd2dfda816f6
Marco Trombetta <marcoltrombetta@gmail.com>
2020-03-21 16:13:37 -0300

    corona

--- coronavirus.py
+++ coronavirus.py
@@ -1,6 +1,7 @@
 #https://www.kaggle.com/allen-institute-for-ai/CORD-19-research-challenge
 #https://stackoverflow.com/questions/11361985/output-data-from-all-columns-in-a-dataframe-in-pandas
 #http://www.stat.yale.edu/Courses/1997-98/101/linreg.htm
+#https://realpython.com/pandas-groupby/
 
 import pandas as pd
 import math
@@ -10,7 +11,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 
-df =  pd.read_csv(r'/data/Python/covid19/COVID19_line_list_data.csv')
+df =  pd.read_csv(r'/home/marco/Downloads/covid19/COVID19_line_list_data.csv')
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 1000)
 
@@ -38,30 +39,25 @@ paramnames = [
 ]
 
 custom_paramnames = [
-'id',
 'age',
-'death'
+'death',
+'country'
 ]
 
 df = pd.DataFrame(df, columns = custom_paramnames)
-df = df.where(((df['death']=='0') | (df['death']=='1'))  & (df['age']>65))
-
-forecast_col = 'id'
-forecast_out = int(math.ceil(0.01*len(df))) #prediction %
-
-df['label'] = df[forecast_col].shift(-forecast_out)
+#df = df.where((df['death']=='1')  & (df['age']>65))
 df.dropna(inplace=True)
-print(df.head(50))
-#features
-X =  np.array(df.drop(['label'],1))
-#labels
-y = np.array(df['label'])
-X = preprocessing.scale(X) #normalized
-y=np.array(df['label'])
+df.drop_duplicates(inplace=True)
 
-X_train, X_test,y_train, y_test=train_test_split(X,y, test_size=0.2)  #correct order x_train, x_test,y_train, y_test
-clf= LinearRegression()
-clf.fit(X_train, y_train) #training the algorithm
-accuracy = clf.score(X_test, y_test) #test
+df2=df.groupby(['country'])['death'].count()
 
-print(accuracy)
+objects = df2
+
+y_pos = np.array(df2)
+performance = df.groupby(['country','death'])['country'].count()
+plt.bar(y_pos, performance, align='center', alpha=0.5)
+plt.xticks(y_pos, objects)
+plt.ylabel('Death')
+plt.title('Programming language usage')
+
+plt.show()
